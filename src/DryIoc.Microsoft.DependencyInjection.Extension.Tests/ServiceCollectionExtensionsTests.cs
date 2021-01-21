4b93623e737edf442decb516495ca970e891bea2
juner <juner@juner.net>
2021-01-21 12:10:36 +0900

    add DryIoc.Microsoft.DepenDencyInjection.Extension

---
+++ src/DryIoc.Microsoft.DependencyInjection.Extension.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,134 @@
+﻿using DryIoc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DryIoc.Microsoft.DependencyInjection.Extension.Tests
+{
+    [TestClass()]
+    public class ServiceCollectionExtensionsTests
+    {
+        static Rules CreateContainerRules() => Rules.Default.WithAutoConcreteTypeResolution()
+            .With(Made.Of(FactoryMethod.ConstructorWithResolvableArguments))
+            .WithDefaultIfAlreadyRegistered(IfAlreadyRegistered.Replace);
+        static IContainer CreateContainer()
+            => new Container(CreateContainerRules());
+        static IContainerExtension CreateContainerExtension()
+            => new DryIocContainerExtension(CreateContainer());
+        [TestMethod()]
+        public void RegisterServices_Transient_Test()
+        {
+            var Container = CreateContainerExtension();
+            IContainerRegistry Registry = Container;
+            Registry.GetContainer().RegisterServices(v =>
+            {
+                v.AddTransient<IA>(v => new A1());
+            });
+            var Values = Container.Resolve<IEnumerable<IA>>().ToList();
+            Assert.AreEqual(1, Values.Count);
+            Assert.IsInstanceOfType(Values[0], typeof(A1));
+
+            var Value = Container.Resolve<IA>();
+            Assert.IsInstanceOfType(Value, typeof(A1));
+        }
+        [TestMethod()]
+        public void RegisterServices_Transient_Many_Test()
+        {
+            var Container = CreateContainerExtension();
+            IContainerRegistry Registry = Container;
+            Registry.GetContainer().RegisterServices(v =>
+            {
+                v.AddTransient<IA>(v => new A1());
+                v.AddTransient<IA, A2>();
+            });
+            var Values = Container.Resolve<IEnumerable<IA>>().ToList();
+            Assert.AreEqual(2, Values.Count);
+            Assert.IsInstanceOfType(Values[0], typeof(A1));
+            Assert.IsInstanceOfType(Values[1], typeof(A2));
+            Assert.ThrowsException<ContainerResolutionException>(() => Container.Resolve<IA>());
+        }
+        [TestMethod()]
+        public void RegisterServices_Singleton_Test()
+        {
+            var Container = CreateContainerExtension();
+            IContainerRegistry Registry = Container;
+            Registry.GetContainer().RegisterServices(v =>
+            {
+                v.AddSingleton<IA>(v => new A1());
+            });
+            var Values = Container.Resolve<IEnumerable<IA>>().ToList();
+            Assert.AreEqual(1, Values.Count);
+            Assert.IsInstanceOfType(Values[0], typeof(A1));
+
+            var Value = Container.Resolve<IA>();
+            Assert.IsInstanceOfType(Value, typeof(A1));
+        }
+        [TestMethod()]
+        public void RegisterServices_Singleton_Many_Test()
+        {
+            var Container = CreateContainerExtension();
+            IContainerRegistry Registry = Container;
+            Registry.GetContainer().RegisterServices(v =>
+            {
+                v.AddSingleton<IA>(v => new A1());
+                v.AddSingleton<IA, A2>();
+            });
+            var Values = Container.Resolve<IEnumerable<IA>>().ToList();
+            Assert.AreEqual(2, Values.Count);
+            Assert.IsInstanceOfType(Values[0], typeof(A1));
+            Assert.IsInstanceOfType(Values[1], typeof(A2));
+            Assert.ThrowsException<ContainerResolutionException>(() => Container.Resolve<IA>());
+        }
+        [TestMethod()]
+        public void RegisterServices_Scoped_Test()
+        {
+            var Container = CreateContainerExtension();
+            IContainerRegistry Registry = Container;
+            Registry.GetContainer().RegisterServices(v =>
+            {
+                v.AddScoped<IA>(v => new A1());
+            });
+            using var Scope = Container.CreateScope();
+            var Values = Scope.Resolve<IEnumerable<IA>>().ToList();
+            Assert.AreEqual(1, Values.Count);
+            Assert.IsInstanceOfType(Values[0], typeof(A1));
+
+            var Value = Scope.Resolve<IA>();
+            Assert.IsInstanceOfType(Value, typeof(A1));
+        }
+        [TestMethod()]
+        public void RegisterServices_Scoped_Many_Test()
+        {
+            var Container = CreateContainerExtension();
+            IContainerRegistry Registry = Container;
+            Registry.GetContainer().RegisterServices(v =>
+            {
+                v.AddScoped<IA>(v => new A1());
+                v.AddScoped<IA, A2>();
+            });
+            using var Scope = Container.CreateScope();
+            var Values = Scope.Resolve<IEnumerable<IA>>().ToList();
+            Assert.AreEqual(2, Values.Count);
+            Assert.IsInstanceOfType(Values[0], typeof(A1));
+            Assert.IsInstanceOfType(Values[1], typeof(A2));
+
+            Assert.ThrowsException<ContainerResolutionException>(() => Container.Resolve<IA>());
+        }
+        interface IA
+        {
+            string Value { get; set; }
+        }
+        class A1 : IA
+        {
+            public string Value { get; set; } = string.Empty;
+        }
+        class A2 : IA
+        {
+            public string Value { get; set; } = string.Empty;
+
+        }
+    }
+}
---
+++ src/DryIoc.Microsoft.DependencyInjection.Extension/ServiceCollectionExtensions.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DryIoc.Microsoft.DependencyInjection.Extension
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IContainer RegisterServices(this IContainer container, Action<IServiceCollection> Action, Func<IRegistrator, ServiceDescriptor, bool> registerDescriptor = null)
+        {
+            var descriptors = new ServiceCollection();
+            Action.Invoke(descriptors);
+            DependencyInjectionAdapter(container, descriptors, registerDescriptor);
+            return container;
+        }
+        static void DependencyInjectionAdapter(IContainer container, IEnumerable<ServiceDescriptor> descriptors = null,
+            Func<IRegistrator, ServiceDescriptor, bool> registerDescriptor = null)
+        {
+            container.Use<IServiceScopeFactory>(r => new DryIocServiceScopeFactory(r));
+            // Registers service collection
+            if (descriptors != null)
+                Populate(container, descriptors, registerDescriptor);
+            var Provider = container.BuildServiceProvider();
+            container.RegisterInstance(Provider);
+        }
+        static void Populate(IContainer container, IEnumerable<ServiceDescriptor> descriptors, Func<IRegistrator, ServiceDescriptor, bool> registerDescriptor = null)
+        {
+            var d = descriptors.GroupBy(v => v.ServiceType).Select(v => (ServiceType: v.Key, Descriptors: v.ToList()));
+            if (registerDescriptor is null)
+                foreach (var (serviceType, ds) in d)
+                    if (ds.Count == 1)
+                        RegisterDescriptor(container, ds.First());
+                    else
+                        foreach (var descriptor in ds)
+                            RegisterDescriptorMany(container, descriptor);
+            else
+                foreach (var (serviceType, ds) in d)
+                    if (ds.Count == 1)
+                    {
+                        if (!registerDescriptor(container, ds.First()))
+                            RegisterDescriptor(container, ds.First());
+                    }
+                    else
+                    {
+                        foreach (var descriptor in ds)
+                            if (!registerDescriptor(container, descriptor))
+                                RegisterDescriptorMany(container, descriptor);
+                    }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="descriptor"></param>
+        static void RegisterDescriptorMany(IContainer container, ServiceDescriptor descriptor)
+        {
+            if (descriptor.ImplementationType is { })
+            {
+                var reuse = descriptor.Lifetime switch
+                {
+                    ServiceLifetime.Singleton => Reuse.Singleton,
+                    ServiceLifetime.Scoped => Reuse.ScopedOrSingleton,
+                    _ => Reuse.Transient
+                };
+                container.Register(descriptor.ServiceType, descriptor.ImplementationType, reuse, ifAlreadyRegistered: IfAlreadyRegistered.AppendNewImplementation);
+            }
+            else if (descriptor.ImplementationFactory is { })
+            {
+                var reuse = descriptor.Lifetime switch
+                {
+                    ServiceLifetime.Singleton => Reuse.Singleton,
+                    ServiceLifetime.Scoped => Reuse.ScopedOrSingleton,
+                    _ => Reuse.Transient
+                };
+                container.RegisterDelegate(true, descriptor.ServiceType, descriptor.ImplementationFactory, reuse, ifAlreadyRegistered: IfAlreadyRegistered.AppendNewImplementation);
+            }
+            else
+            {
+                container.RegisterInstance(true, descriptor.ServiceType, descriptor.ImplementationInstance, ifAlreadyRegistered: IfAlreadyRegistered.AppendNewImplementation);
+            }
+        }
+        static void RegisterDescriptor(IContainer container, ServiceDescriptor descriptor)
+        {
+            if (descriptor.ImplementationType is { })
+            {
+                var reuse = descriptor.Lifetime switch
+                {
+                    ServiceLifetime.Singleton => Reuse.Singleton,
+                    ServiceLifetime.Scoped => Reuse.ScopedOrSingleton,
+                    _ => Reuse.Transient
+                };
+                container.Register(descriptor.ServiceType, descriptor.ImplementationType, reuse);
+            }
+            else if (descriptor.ImplementationFactory is { })
+            {
+                var reuse = descriptor.Lifetime switch
+                {
+                    ServiceLifetime.Singleton => Reuse.Singleton,
+                    ServiceLifetime.Scoped => Reuse.ScopedOrSingleton,
+                    _ => Reuse.Transient
+                };
+                container.RegisterDelegate(true, descriptor.ServiceType, descriptor.ImplementationFactory, reuse);
+            }
+            else
+            {
+                container.RegisterInstance(true, descriptor.ServiceType, descriptor.ImplementationInstance);
+            }
+        }
+    }
+}
